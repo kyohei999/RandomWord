749cd19c50248d53563e4bdea2d88897609c5a51
kyohei999 <66673838+kyohei999@users.noreply.github.com>
2021-06-05 13:37:55 +0900

    コード変化

--- RundomWordBot/src/main/java/com/example/demo/RundomWordBotApplication.java
+++ RundomWordBot/src/main/java/com/example/demo/RundomWordBotApplication.java
@@ -3,11 +3,31 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
 @SpringBootApplication
+@LineMessageHandler
 public class RundomWordBotApplication {
 
 	public static void main(String[] args) {
         SpringApplication.run(RundomWordBotApplication.class, args);
+    }
+
+	@EventMapping
+    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        String originalMessageText = event.getMessage().getText();
+        return new TextMessage(originalMessageText);
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
     }
 
 
